6f5c011b9b221cd5641ae293cccf9f259782ee28
unknown <saymongrs@gmail.com>
2021-04-14 22:02:32 -0300

    ajuste na tela index para exibição dos link dos testes

--- src/main/java/com/example/demo/Controller/HomeRestController.java
+++ src/main/java/com/example/demo/Controller/HomeRestController.java
@@ -12,7 +12,8 @@ public class HomeRestController {
 	
 	@GetMapping
 	private ResponseEntity<Object> home() {
-		return ResponseEntity.ok().body("demo teste. Mais detalhe em: <a target='_blank' href='https://github.com/saymon83/demo'>https://github.com/saymon83/demo<a>");
+		return ResponseEntity.ok().body("demo teste. Mais detalhe em: <a target='_blank' href='https://github.com/saymon83/demo'>https://github.com/saymon83/demo<a><br> "
+				+ "acesso aos testes <a target='_blank' href='http://localhost:8089/primeira-venda'>http://localhost:8089/primeira-venda<a> , <a target='_blank' href='http://localhost:8089/segunda-venda'>http://localhost:8089/segunda-venda<a> , <a target='_blank' href='http://localhost:8089/terceira-venda'>http://localhost:8089/terceira-venda<a> ");
 	}
 
 }
